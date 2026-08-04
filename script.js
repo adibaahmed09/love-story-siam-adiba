1481c803b881f28fdb0a5012724b2f036dea0c24
adibaahmed09 <adibaahammad9@gmail.com>
2026-08-04 17:10:09 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,220 @@
+let currentQuestion = 0;
+
+
+const questions = [
+
+"Do you love me? 💖",
+
+"Do you miss me when I am not around? 🥺",
+
+"Do you like our memories together? 📸",
+
+"Do I make you happy? 😊",
+
+"Do you enjoy my surprises? 🎁",
+
+"Will you always support me? ❤️",
+
+"Do you like spending time with me? 💕",
+
+"Am I special to you? ✨",
+
+"Do you want more beautiful memories with me? 🌸",
+
+"Are you ready for your final surprise? 💌"
+
+];
+
+
+
+// PASSWORD
+
+function checkPassword(){
+
+let pass =
+document.getElementById("password").value;
+
+
+if(pass === "01012023"){
+
+
+document.getElementById("passwordPage")
+.classList.add("hidden");
+
+
+document.getElementById("envelopePage")
+.classList.remove("hidden");
+
+
+document.getElementById("music").play();
+
+
+}
+
+else{
+
+
+document.getElementById("message").innerHTML =
+"🥺 Teddy is crying... Wrong password 💔";
+
+
+document.getElementById("characterReaction")
+.innerHTML="😢";
+
+
+}
+
+}
+
+
+
+
+// OPEN ENVELOPE
+
+function openQuestions(){
+
+
+document.getElementById("envelopePage")
+.classList.add("hidden");
+
+
+document.getElementById("questionPage")
+.classList.remove("hidden");
+
+
+showQuestion();
+
+
+}
+
+
+
+
+function showQuestion(){
+
+document.getElementById("questionText")
+.innerHTML =
+questions[currentQuestion];
+
+}
+
+
+
+
+// YES BUTTON
+
+function yesAnswer(){
+
+document.getElementById("characterReaction")
+.innerHTML="🤗💖";
+
+
+document.getElementById("reaction")
+.innerHTML=
+"Sending you a big hug & love 💕";
+
+
+nextQuestion();
+
+}
+
+
+
+
+// NO BUTTON
+
+function noAnswer(){
+
+document.getElementById("characterReaction")
+.innerHTML="😭";
+
+
+document.getElementById("reaction")
+.innerHTML=
+"Teddy is crying 🥺";
+
+
+nextQuestion();
+
+}
+
+
+
+
+
+function nextQuestion(){
+
+
+currentQuestion++;
+
+
+if(currentQuestion < questions.length){
+
+
+setTimeout(()=>{
+
+showQuestion();
+
+},800);
+
+
+}
+
+else{
+
+
+setTimeout(()=>{
+
+
+document.getElementById("questionPage")
+.classList.add("hidden");
+
+
+document.getElementById("letterPage")
+.classList.remove("hidden");
+
+
+},1000);
+
+
+}
+
+}
+
+
+
+
+
+// MEMORY PAGE
+
+function showMemories(){
+
+
+document.getElementById("letterPage")
+.classList.add("hidden");
+
+
+document.getElementById("memoryPage")
+.classList.remove("hidden");
+
+
+}
+
+
+
+
+
+// GIFT PAGE
+
+function showGift(){
+
+
+document.getElementById("memoryPage")
+.classList.add("hidden");
+
+
+document.getElementById("giftPage")
+.classList.remove("hidden");
+
+
+}
