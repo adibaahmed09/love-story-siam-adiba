c5c65b98a92255afc129d4d36146f7b88d169bf3
adibaahmed09 <adibaahammad9@gmail.com>
2026-08-04 18:30:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,12 @@ const questions = [
 "Are you ready for your final surprise? 💌"
 
 ];
+document.getElementById("questionPage")
+.classList.add("hidden");
 
+
+document.getElementById("letterPage")
+.classList.remove("hidden");
 
 
 // PASSWORD
